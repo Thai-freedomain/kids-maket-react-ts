acb2f3371ea20792966616e52da10971dbefd149
Thai-freedomain <114232724+Thai-freedomain@users.noreply.github.com>
2023-05-05 23:32:26 +0700

    prettier, globalstyles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,23 @@
-import clsx from 'clsx'
-import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-
-
+import clsx from 'clsx';
+import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Header() {
-    return ( 
-      <div className='container'>
-        <div className='container-nav'>
-            <div >
-                <Link to={'/'}>
-                    Trang chủ
-                </Link>
-            </div>
-        <Link to={'/account'}>Account</Link>
-        <Link to={'/store'}>Store</Link>
+  return (
+    <div className="container">
+      <div className="container-nav">
+        <div className={clsx()}>
+          <Link to={'/'}>Trang chủ</Link>
         </div>
-                
+        <div>
+          <Link to={'/account'}>Account</Link>
+        </div>
+        <div>
+          <Link to={'/store'}>Store</Link>
+        </div>
       </div>
-     );
+    </div>
+  );
 }
 
 export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import {BrowserRouter as Router} from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import GlobalStyles from './components/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <GlobalStyles>
+        <App />
+      </GlobalStyles>
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
